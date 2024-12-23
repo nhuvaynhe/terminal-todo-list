76a9e966d92a5d0b62658b15b05cdd0849c0b63c
nhuvaynhe <nndai0603@gmail.com>
2024-12-23 16:38:33 +0700

    add: add enum for command and process

--- enum.h
+++ enum.h
@@ -1,12 +1,38 @@
+typedef int enumbool;
+
 typedef enum KeyProcess {
     INSERT = 'i',
-    DELETE = 'd',
+    DELETE_TASK = 'd',
     CHANGE_STATUS = 'c',
     EDIT_TASK = 'e'
 } KeyProcess;
+
+typedef enum MainProc {
+    Proc_InsertTask,
+    Proc_DeleteTask,
+    Proc_ChangeStatus,
+    Proc_EditTask,
+
+    Proc_None
+} MainProc;
+
+typedef enum ProcCommand {
+    PROC_CMD_INIT,
+    PROC_CMD_PROCESS,
+    PROC_CMD_END,
+
+    PROC_CMD_NONE,
+} ProcCommand;
+
+typedef enum gfBoolean {
+    eTRUE,
+    eFALSE,
+}  gfBoolean;
 
 typedef enum TaskStatus {
     PENDING,
     IN_PROGRESS,
     COMPLETED,
 } TaskStatus;
+
+
